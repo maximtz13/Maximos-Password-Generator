6a44cb076cb1714a4f85a755a98ca873d562e3f9
Maximoo <maximtz99@gmail.com>
2022-07-31 20:57:48 -0500

    corrected variables added to JS code

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,6 @@ var criteria = [];
 var password = "";
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = lowerCase.map(element => { return element.toUpperCase(); });
-console.log(upperCase);
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specChar = ['/', '[', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\\', '-', '=', '{', '}', ';', ':', '"', '|', ',', '.', '<', '>', '?', '+', ']'];
 var length = 0;
