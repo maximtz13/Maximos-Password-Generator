c0f427ff36863dd1d8e3df7e8ea00e5116851585
Maximoo <maximtz99@gmail.com>
2022-07-31 22:21:33 -0500

    added first prompt/Password-length with validation

--- Develop/script.js
+++ Develop/script.js
@@ -7,10 +7,23 @@ var upperCase = lowerCase.map(element => { return element.toUpperCase(); });
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specChar = ['/', '[', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\\', '-', '=', '{', '}', ';', ':', '"', '|', ',', '.', '<', '>', '?', '+', ']'];
 var length = 0;
+
 // Create Initial Function
-// Prompt Password Length (prompt if not met)(Validate)
-// Prompt Ask for Character Types (minimum 1, prompt if not met)(Validate)
-// After All Prompts Generate Password On Page
+var generatePassword = function () {
+  // Prompt Password Length (prompt if not met)(Validate)
+  while (!(length >= 8 && length <= 128)) {
+    length = window.prompt("Choose a password length. (between 8 to 128 characters long)");
+    if (!(length >= 8 && length <= 128)) {
+      window.alert("Choose a number between 8-128.");
+    } else { 
+      // Validation
+      window.alert("Thank you for choosing a password length."); 
+      continue }
+  }
+
+  // Prompt Ask for Character Types (minimum 1, prompt if not met)(Validate)
+  // After All Prompts Generate Password On Page
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
