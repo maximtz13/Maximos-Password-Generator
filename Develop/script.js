6b8476003e44d9459264a9eac3645def4ea7b116
Maximoo <maximtz99@gmail.com>
2022-07-31 20:52:29 -0500

    added variables to the code

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
-
+// Add Variables
+var criteria = [];
+var password = "";
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCase = lowerCase.map(element => { return element.toUpperCase(); });
+console.log(upperCase);
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specChar = ['/', '[', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\\', '-', '=', '{', '}', ';', ':', '"', '|', ',', '.', '<', '>', '?', '+', ']'];
+var length = 0;
 // Create Initial Function
 // Prompt Password Length (prompt if not met)(Validate)
 // Prompt Ask for Character Types (minimum 1, prompt if not met)(Validate)
