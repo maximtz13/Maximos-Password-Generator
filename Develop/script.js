e3874cb5cd56ad391817470345ee69e225aa12f5
Maximoo <maximtz99@gmail.com>
2022-07-31 19:01:07 -0500

    added initial pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
 
+// Create Initial Function
+// Prompt Password Length (prompt if not met)(Validate)
+// Prompt Ask for Character Types (minimum 1, prompt if not met)(Validate)
+// After All Prompts Generate Password On Page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
