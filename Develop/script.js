531d59c94ea7c6395efc1083a256a91b84fcc639
Maximoo <maximtz99@gmail.com>
2022-08-02 23:16:14 -0500

    working on javascript code

--- Develop/script.js
+++ Develop/script.js
@@ -3,17 +3,19 @@
 var criteria = [];
 var password = "";
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCase = lowerCase.map(element => { return element.toUpperCase(); });
+var upperCase = lowerCase.map(function (a) {
+  return a.toUpperCase();
+});
 var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specChar = ['/', '[', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\\', '-', '=', '{', '}', ';', ':', '"', '|', ',', '.', '<', '>', '?', '+', ']'];
-var length = 0;
+var lengthP = 0;
 
 // Create Initial Function
 var generatePassword = function () {
   // Prompt Password Length (prompt if not met)(Validate)
-  while (!(length >= 8 && length <= 128)) {
-    length = window.prompt("Choose a password length. (between 8 to 128 characters long)");
-    if (!(length >= 8 && length <= 128)) {
+  while (!(lengthP >= 8 && lengthP <= 128)) {
+    lengthP = window.prompt("Choose a password length. (between 8 to 128 characters long)");
+    if (!(lengthP >= 8 && lengthP <= 128)) {
       window.alert("Choose a number between 8-128.");
     } else {
       // Validation
@@ -25,8 +27,8 @@ var generatePassword = function () {
   // Prompt Ask for Character Types (minimum 1, prompt if not met)(Validate)
   var charTypes = function () {
     // Start of Loop for Character Type Until at Least One Type is Chosen
-    while (criteria.length === 0) {
-      var passwordCrit =""
+    while (criteria.length === 0 || criteria.length === "") {
+      var passwordCrit = ""
       // choose a character type
       window.alert("At least one character type must be chosen in order to generate password.");
       lowerFun();
@@ -35,19 +37,19 @@ var generatePassword = function () {
         var promptLower = window.prompt('Would you like to include lowercase characters in your password? Enter "YES" or "NO" to choose.');
 
         if (promptLower === "" || promptLower === null) {
-          window.alert('"You must enter "YES" or "NO" to proceed! Please try again.');
+          window.alert('You must enter "YES" or "NO" to proceed! Please try again.');
           return lowerFun();
         }
 
         promptLower = promptLower.toLowerCase();
-        if (promptLower === "NO") {
+        if (promptLower === "no") {
           window.alert("Lowercase characters have been skipped.");
           upperFun();
         }
 
-        if (promptLower === "YES") {
+        if (promptLower === "yes") {
           window.alert("Lowercase characters have been added.");
-          criteria.concat(lowerCase);
+          criteria+=lowerCase;
           passwordCrit += lowerCase[Math.floor(Math.random() * lowerCase.length)];
           upperFun();
         }
@@ -58,22 +60,22 @@ var generatePassword = function () {
         var promptUpper = window.prompt('Would you like to include uppercase characters in your password? Enter "YES" or "NO" to choose.');
 
         if (promptUpper === "" || promptUpper === null) {
-          window.alert('"You must enter "YES" or "NO" to proceed! Please try again.');
+          window.alert('You must enter "YES" or "NO" to proceed! Please try again.');
           return upperFun();
-        }
+        };
 
         promptUpper = promptUpper.toLowerCase();
-        if (promptUpper === "NO") {
+        if (promptUpper === "no") {
           window.alert("Uppercase characters have been skipped.");
           numberFun();
-        }
+        };
 
-        if (promptUpper === "YES") {
+        if (promptUpper === "yes") {
           window.alert("Uppercase characters have been added.");
-          criteria.concat(upperCase);
+          criteria+=upperCase;
           passwordCrit += upperCase[Math.floor(Math.random() * upperCase.length)];
           numberFun();
-        }
+        };
       };
 
       numberFun();
@@ -81,21 +83,21 @@ var generatePassword = function () {
         var promptNumber = window.prompt('Would you like to include number characters in your password? Enter "YES" or "NO" to choose.');
 
         if (promptNumber === "" || promptNumber === null) {
-          window.alert('"You must enter "YES" or "NO" to proceed! Please try again.');
+          window.alert('You must enter "YES" or "NO" to proceed! Please try again.');
           return numberFun();
         }
 
         promptNumber = promptNumber.toLowerCase();
-        if (promptNumber === "NO") {
+        if (promptNumber === "no") {
           window.alert("Number characters have been skipped.");
           specFun();
         }
 
-        if (promptNumber === "YES") {
+        if (promptNumber === "yes") {
           window.alert("Number characters have been added.");
-          criteria.concat(num);
+          criteria = +num;
           passwordCrit += num[Math.floor(Math.random() * num.length)];
-          specFun;
+          specFun();
         }
       };
 
@@ -104,38 +106,50 @@ var generatePassword = function () {
         var promptSpec = window.prompt('Would you like to include special characters in your password? Enter "YES" or "NO" to choose.');
 
         if (promptSpec === "" || promptSpec === null) {
-          window.alert('"You must enter "YES" or "NO" to proceed! Please try again.');
+          window.alert('You must enter "YES" or "NO" to proceed! Please try again.');
           return specFun();
         }
 
         promptSpec = promptSpec.toLowerCase();
-        if (promptSpec === "NO") {
+        if (promptSpec === "no") {
           window.alert("Special characters have been skipped.");
           finishedRes();
         }
 
-        if (promptSpec === "YES") {
+        if (promptSpec === "yes") {
           window.alert("Special characters have been added.");
-          criteria.concat(specChar);
+          criteria += specChar;
           passwordCrit += specChar[Math.floor(Math.random() * specChar.length)];
           finishedRes();
         }
+        console.log(criteria);
       };
-      break;
-    }
+    };
     finishedRes();
     function finishedRes() {
-    window.alert("Password criteria successfully set, Thank You!")
-    return passwordCrit;
+      window.alert("Password criteria successfully set, Thank You!")
+      return passwordCrit;
+    };
   };
+
+  password += charTypes();
+
+  charOptions = [];
+
+  for (i = 0; i < criteria.length; i++) {
+    if (criteria[i] === ',') {
+      continue;
+    }
+    else {
+      charOptions += criteria[i];
+    }
   }
 
-  password+=charTypes();
-  var charLength = length - password.length;
+  var charLength = lengthP - password.length;
 
-  // for (let i=0; i<charLength; i++) {
-  //   password+=charOptions[Math.floor(Math.random() * charOptions.length)];
-  // }
+  for (let i = 0; i < charLength; i++) {
+    password += charOptions[Math.floor(Math.random() * charOptions.length)];
+  }
 
   return password;
 };
